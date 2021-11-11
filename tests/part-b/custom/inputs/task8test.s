a33586916f67af2616bddcbe3c7e00ba57b77ba9
Meilong Zhang <meilong.zhang@berkeley.edu>
2021-11-10 22:37:50 -0800

    finish task 12

--- tests/part-b/custom/inputs/task8test.s
+++ tests/part-b/custom/inputs/task8test.s
@@ -3,12 +3,10 @@ addi t1, x0, 5
 bne t0, t1, result
 beq t0, t1, result
 addi s1, x0, 155
-result:
-addi s0, x0, 154
+result: addi s0, x0, 154
 addi t1, x0, -3
 blt t0, t1, result2
 bge t1, t0, result2
 bltu t0, t1, result2
 addi s1, x0, 0
-result2:
-addi s0, x0, 2
+result2: addi s0, x0, 2
